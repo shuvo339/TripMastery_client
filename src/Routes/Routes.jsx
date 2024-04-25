52bb5575d478a03bf3d7786eca1238d9a0944c7d
shuvo339 <shubhroshafiullah@gmail.com>
2024-04-25 21:40:29 +0600

    basic project setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,14 @@
+import {createBrowserRouter} from "react-router-dom";
+
+ export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+      children: [
+        {
+          path: "/",
+          element: ,
+        },
+      ],
+    },
+  ]);
