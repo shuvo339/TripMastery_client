e804c8d5e017c691c42a1640d081aebfbf555d1b
shuvo339 <shubhroshafiullah@gmail.com>
2024-04-26 05:56:03 +0600

    environment variable applied

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDPCjs5dDnEedKrcc1odhni9VF3kxfbEoM",
-  authDomain: "tripmastery-auth.firebaseapp.com",
-  projectId: "tripmastery-auth",
-  storageBucket: "tripmastery-auth.appspot.com",
-  messagingSenderId: "689190575516",
-  appId: "1:689190575516:web:898af1a602b99ef7ebbc59"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
